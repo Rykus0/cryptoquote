a1a8772c299643a610cb6b7032f6443641703807
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-06-10 17:22:34 -0400

    test: add reducer newGame test

--- __tests__/reducer.test.ts
+++ __tests__/reducer.test.ts
@@ -1,4 +1,9 @@
-import reducer, { initialState, ActionType } from "../src/app/state/reducer";
+import reducer, {
+  initialState,
+  ActionType,
+  normalizeQuote,
+} from "../src/app/state/reducer";
+import { applyCypher } from "../src/utils/cypher";
 
 describe("Reducer", () => {
   describe("Tick", () => {
@@ -37,6 +42,63 @@ describe("Reducer", () => {
 
       expect(state.msElapsed).toBe(1000);
       expect(state.lastTick).toBe(2000);
+    });
+  });
+
+  describe("NewGame", () => {
+    const quote = "This İs a test quøte.";
+    const author = "Test Ç. Àuthor";
+
+    jest.spyOn(Date, "now").mockReturnValue(1000);
+
+    const state = reducer(initialState, {
+      type: ActionType.NewGame,
+      payload: {
+        quote,
+        author,
+      },
+    });
+
+    it("should create a new cypher", () => {
+      expect(state.cypher).toBeInstanceOf(Map);
+    });
+
+    it("should create a new answer cypher", () => {
+      expect(applyCypher("i", state.answerCypher)).toBe("");
+    });
+
+    it("should record the original quote", () => {
+      expect(state.quote).toBe(quote + " - " + author);
+    });
+
+    it("should encrypt the quote", () => {
+      expect(state.encryptedQuote).toEqual(
+        applyCypher(normalizeQuote(state.quote), state.cypher)
+      );
+    });
+
+    it("should record the encrypted letter frequency", () => {
+      expect(state.letterFrequency.get(applyCypher("t", state.cypher))).toBe(7);
+    });
+
+    it("should reset the timer", () => {
+      expect(state.msElapsed).toBe(0);
+    });
+
+    it("should reset the last tick", () => {
+      expect(state.lastTick).toBe(1000);
+    });
+
+    it("should clear the current highlighted letter", () => {
+      expect(state.currentLetter).toBe("");
+    });
+
+    it("should reset the loading state", () => {
+      expect(state.loading).toBe(false);
+    });
+
+    it("should reset the win state", () => {
+      expect(state.win).toBe(false);
     });
   });
 });
