6b0b2eae52c6275a9f5359913ad80a31f89ce008
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-06-24 17:31:42 -0400

    refactor: move controls into separate component

---
+++ src/app/components/Controls.tsx
@@ -0,0 +1,26 @@
+import Button from "@/app/components/Button";
+import styles from "./Controls.module.css";
+
+type ControlsProps = {
+  gameOff: boolean;
+  msElapsed: number;
+
+  onNewGame: () => void;
+  onClear: () => void;
+  onRevealAll: () => void;
+  onRevealCurrent: () => void;
+};
+
+export default function Controls(props: ControlsProps) {
+  return (
+    <div className={styles.controls}>
+      <Button onClick={props.onNewGame}>New game</Button>
+      <Button onClick={props.onClear} disabled={props.gameOff}>
+        Clear
+      </Button>
+      <Button onClick={props.onRevealAll} disabled={props.gameOff}>
+        Give up
+      </Button>
+    </div>
+  );
+}
