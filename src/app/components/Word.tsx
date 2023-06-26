10481657070e84f129c711404a8df9b3c2a9e6b6
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-06-26 09:36:15 -0400

    refactor: move letters into words

--- src/app/components/Word.tsx
+++ src/app/components/Word.tsx
@@ -1,6 +1,32 @@
-import { type PropsWithChildren } from "react";
+import { type ChangeEvent, type FocusEvent, type KeyboardEvent } from "react";
+import Letter from "@/app/components/Letter";
 import styles from "./Word.module.css";
 
-export default function Word(props: PropsWithChildren) {
-  return <span className={styles.word}>{props.children}</span>;
+type WordProps = {
+  value: string;
+  letterFrequency: Map<string, number>;
+  onLetterChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onLetterFocus: (e: FocusEvent<HTMLInputElement>) => void;
+  onLetterKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
+  currentLetter?: string;
+  letterValues: Map<string, string>;
+};
+
+export default function Word(props: WordProps) {
+  return (
+    <span className={styles.word}>
+      {props.value.split("").map((char: string, charIdx: number) => (
+        <Letter
+          key={[props.value, "letter", char, charIdx].join("-")}
+          char={char}
+          occurrences={props.letterFrequency.get(char) || 1}
+          onChange={props.onLetterChange}
+          onFocus={props.onLetterFocus}
+          onKeyDown={props.onLetterKeyDown}
+          highlighted={props.currentLetter === char}
+          value={props.letterValues.get(char)}
+        />
+      ))}
+    </span>
+  );
 }
