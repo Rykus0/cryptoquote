586cb26cf2d53068dccbc7829c7f6dc8ac5d2ee1
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-01 12:54:21 -0400

    feat: add share component

---
+++ src/app/components/Share.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import Button from "@/app/components/Button";
+
+type ShareProps = {
+  content: string;
+};
+
+export default function Share(props: ShareProps) {
+  const [message, setMessage] = useState<string>("");
+
+  async function share() {
+    const shareData = {
+      title: `I decyphered this quote!`,
+      text: props.content,
+      url: window.location.href,
+    };
+
+    setMessage("");
+
+    try {
+      await navigator.share(shareData);
+      setMessage("Content shared successfully");
+    } catch (err) {
+      try {
+        await navigator.clipboard.writeText(
+          `${props.content} \n\n ${window.location.href}`
+        );
+        setMessage("Content copied to clipboard");
+      } catch (err) {
+        setMessage(
+          `Error sharing content - please copy manually: ${window.location.href}`
+        );
+        console.error(err);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Button onClick={share}>Share</Button>
+      <div role="status" aria-live="polite">
+        {message}
+      </div>
+    </div>
+  );
+}
