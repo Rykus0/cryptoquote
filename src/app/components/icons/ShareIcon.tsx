d5e0a57dcc8941e9382ee669195bb9b3c9c8333a
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-02 19:46:16 -0400

    feat: add share icon

---
+++ src/app/components/icons/ShareIcon.tsx
@@ -0,0 +1,17 @@
+export default function ShareIcon() {
+  return (
+    <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" fill="none">
+      <path
+        d="M41 49H15C12.2386 49 10 51.2386 10 54V113C10 115.761 12.2386 118 15 118H113C115.761 118 118 115.761 118 113V54C118 51.2386 115.761 49 113 49H88"
+        stroke="currentColor"
+        stroke-width="10"
+      />
+      <path
+        d="M64 82L64 45.5L64 9M64 9L43.5 29.5M64 9L84.5 29.5"
+        stroke="currentColor"
+        stroke-width="10"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
