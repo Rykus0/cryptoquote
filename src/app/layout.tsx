5344db50fa35104e015635a674babf640476be3c
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-02 19:51:34 -0400

    feat: update global metadata with OG content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,25 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
+const domain = "cryptoquote-five.vercel.app";
 
 export const metadata = {
   title: "Cryptoquotle",
   description: "A classic game of decyphering encrypted famous quotes.",
+  openGraph: {
+    title: "Cryptoquotle",
+    description: "A classic game of decyphering encrypted famous quotes",
+    url: `https://${domain}`,
+    type: "website",
+    images: [
+      {
+        url: `https://${domain}/cryptoquotle.png`,
+        width: 180,
+        height: 180,
+        alt: "Online Cryptoquote Game",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
