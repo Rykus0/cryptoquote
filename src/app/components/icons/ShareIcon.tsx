a481fda70975b39921759f1be0da2f92cb49b7ea
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-02 20:06:18 -0400

    feat: add accessibility attrs to share icon

--- src/app/components/icons/ShareIcon.tsx
+++ src/app/components/icons/ShareIcon.tsx
@@ -1,6 +1,13 @@
 export default function ShareIcon() {
   return (
-    <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" fill="none">
+    <svg
+      viewBox="0 0 128 128"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      role="img"
+      aria-hidden="true"
+    >
+      <title>Share</title>
       <path
         d="M41 49H15C12.2386 49 10 51.2386 10 54V113C10 115.761 12.2386 118 15 118H113C115.761 118 118 115.761 118 113V54C118 51.2386 115.761 49 113 49H88"
         stroke="currentColor"
