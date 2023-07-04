c841c7e4e32fbdc35d227b776581303d1ced472b
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-04 16:29:18 -0400

    feat: add more spacing in logo

--- src/app/components/icons/LogoIcon.tsx
+++ src/app/components/icons/LogoIcon.tsx
@@ -12,18 +12,18 @@ export function LogoIcon() {
       aria-hidden="true"
     >
       <title>Cryptoquotle Logo</title>
-      <circle cx="56" cy="53" r="36" stroke="black" strokeWidth="12" />
+      <circle cx="56" cy="53" r="36" strokeWidth="12" />
       <rect
-        x="74.3684"
-        y="59.5534"
-        width="60"
-        height="12"
-        transform="rotate(54.5202 74.3684 59.5534)"
+        x="73.672"
+        y="58.374"
+        width="61.3646"
+        height="12.2349"
+        transform="rotate(54.5202 73.672 58.374)"
       />
       <rect
         x="108.875"
         y="108.593"
-        width="25"
+        width="24"
         height="8"
         transform="rotate(144.52 108.875 108.593)"
       />
@@ -35,7 +35,7 @@ export function LogoIcon() {
         transform="rotate(144.52 95.0208 98.866)"
       />
       <mask
-        id="mask0_1_2"
+        id="mask0_12_82"
         style={{ maskType: "alpha" }}
         maskUnits="userSpaceOnUse"
         x="23"
@@ -45,8 +45,8 @@ export function LogoIcon() {
       >
         <path d="M23 25H76L56 53L76 81H56H23V25Z" />
       </mask>
-      <g mask="url(#mask0_1_2)">
-        <circle cx="56" cy="53" r="22" strokeWidth="12" />
+      <g mask="url(#mask0_12_82)">
+        <circle cx="56.5" cy="53.5" r="19.25" strokeWidth="10.5" />
       </g>
     </svg>
   );
