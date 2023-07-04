ff523ef9709e60442ebb44ff79a5abc1e096f591
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-07-04 11:00:22 -0400

    feat: add utility for storing and retrieving history

--- src/app/state/storage.ts
+++ src/app/state/storage.ts
@@ -1,16 +1,17 @@
 import { type State } from "./types";
 
-const STORAGE_KEY = "savedGame";
+const GAME_KEY = "savedGame";
+const HISTORY_KEY = "gameHistory";
 
 export function saveGame(data: State) {
   try {
-    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(data, replacer));
+    window.localStorage.setItem(GAME_KEY, JSON.stringify(data, replacer));
   } catch (error) {}
 }
 
 export function loadGame(): State | null {
   try {
-    const data = window.localStorage.getItem(STORAGE_KEY);
+    const data = window.localStorage.getItem(GAME_KEY);
 
     if (data) {
       return JSON.parse(data, reviver);
@@ -24,7 +25,7 @@ export function loadGame(): State | null {
 
 export function deleteGame() {
   try {
-    window.localStorage.removeItem(STORAGE_KEY);
+    window.localStorage.removeItem(GAME_KEY);
   } catch (error) {}
 }
 
@@ -41,4 +42,41 @@ function reviver(key: string, value: unknown) {
     return new Map(value as []);
   }
   return value;
+}
+
+// -------------------------------------
+
+type HistoricalGame = {
+  id: string;
+  quote: string;
+  author: string;
+  msElapsed: number;
+  win: boolean;
+  date: Date;
+};
+
+type History = HistoricalGame[];
+
+export function addHistory(data: HistoricalGame) {
+  try {
+    const history = getHistory() ?? [];
+    history.unshift(data);
+    window.localStorage.setItem(HISTORY_KEY, JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function getHistory(): History | null {
+  try {
+    const data = window.localStorage.getItem(HISTORY_KEY);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+  } catch (error) {
+    return null;
+  }
+
+  return null;
 }
